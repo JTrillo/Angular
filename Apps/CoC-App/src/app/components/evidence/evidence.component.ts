157574b4baef5d0cf97f6fda11b37c6f722b338a
JTrillo <jokinator20@gmail.com>
2019-01-23 18:33:46 +0100

    Starting modal to transfer evidences in evidence component.

--- Apps/CoC-App/src/app/components/evidence/evidence.component.ts
+++ Apps/CoC-App/src/app/components/evidence/evidence.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { UserDataService } from 'src/app/services/user-data.service';
-import { Evidence } from 'src/app/services/hyperledger.service';
+import { Evidence, Profile } from 'src/app/services/hyperledger.service';
+
+declare var jQuery:any; //To import jQuery
 
 @Component({
   selector: 'app-evidence',
@@ -15,6 +17,10 @@ export class EvidenceComponent implements OnInit {
   evidence:Evidence;
   additionDate:string;
 
+  participants:Profile[] = []; //Participants in case that no are the current user
+
+  @ViewChild('closeModal') private closeModal: ElementRef;
+
   constructor(private activatedroute:ActivatedRoute,
               private userdata:UserDataService,
               private location:Location) {
@@ -22,14 +28,16 @@ export class EvidenceComponent implements OnInit {
       let evidence_id = params['evi_id'];
       this.evidence = this.userdata.getEvidence(evidence_id);
       this.additionDate = this.dateToInputDate(this.evidence.additionDate);
+      this.participants = this.removeCurrentUser(this.evidence.case.participants);
     });
   }
 
   ngOnInit() {
   }
 
-  openModalTransfer(){
-    console.log("openModalTransfer pressed");
+  transferEvidence(){
+    console.log("transferEvidence pressed");
+    jQuery('#transferModal').modal('hide');
   }
 
   downloadCopy(){
@@ -44,6 +52,17 @@ export class EvidenceComponent implements OnInit {
     let month = date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1;
     let aux = `${date.getFullYear()}-${month}-${date.getDate()}`;
     return aux;
+  }
+
+  private removeCurrentUser(participants:Profile[]): Profile[]{
+    let user_id = this.userdata.getUserProfile().identifier;
+    for(let aux = 0; aux<participants.length; aux++){
+      if(user_id == participants[aux].identifier){
+        participants.splice(aux, 1);
+        return participants;
+      }
+    }
+    return participants;
   }
 
 }
--- Apps/CoC-App/src/app/services/hyperledger.service.ts
+++ Apps/CoC-App/src/app/services/hyperledger.service.ts
@@ -25,6 +25,15 @@ export class HyperledgerService {
       job: "Detective",
       studies: "Software Engineer",
       office: "Málaga"
+    },
+    { identifier: "12346",
+      firstName: "Clark",
+      lastName: "Firewall",
+      birthdate: new Date(1994,4,25),
+      gender: "Male",
+      job: "Officer",
+      studies: "Software Engineer",
+      office: "Málaga"
     }
   ];
 
@@ -36,7 +45,7 @@ export class HyperledgerService {
       closureDate: new Date(),*/
       status:"OPENED",
       openedBy:this.userProfiles[0],
-      participants:[this.userProfiles[0], this.userProfiles[1]]
+      participants:[this.userProfiles[0], this.userProfiles[1], this.userProfiles[2]]
     }
   ];
 
@@ -52,7 +61,7 @@ export class HyperledgerService {
   
   private userEvidences:Evidence[] = [
     { identifier:"EVD001",
-      hash:"A156456156D4351545F6",
+      hash:"cf23df2207d99a74fbe169e3eba035e633b65d94",
       hashType:"SHA-1",
       description:"Video recording",
       additionDate:new Date(2018,10,20),
@@ -60,7 +69,7 @@ export class HyperledgerService {
       olderOwners:this.owners,
       case:this.userCases[0]},
     { identifier:"EVD002",
-      hash:"A156456156D4351545F6",
+      hash:"cf23df2207d99a74fbe169e3eba035e633b65d94",
       hashType:"SHA-1",
       description:"Image",
       additionDate:new Date(2018,10,21),
